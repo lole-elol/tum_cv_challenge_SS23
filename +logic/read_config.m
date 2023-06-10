06dd03e2263f35ddabae8115785b5e644ae6fe6d
Lorenz Leinfelder <loleider@gmx.de>
2023-06-10 17:22:42 +0200

    move

--- +logic/read_config.m
+++ +logic/read_config.m
@@ -3,6 +3,6 @@
 function config = read_config(config_file, config_type)
 % Reads a config.json  and returns a struct with the config values
 
-config = jsondecode(fileread(config_file)).(config_type)
+config = jsondecode(fileread(config_file)).(config_type);
 
 end
