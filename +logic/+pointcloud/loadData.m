909ae69eebb99c0c24b9b6da97bfcbd72ba73938
Lukas Schüttler <lukas.schuettler@ise-technology.com>
2023-06-15 12:20:00 +0200

    add cluster filtering, add pc outlier filter

--- +logic/+pointcloud/loadData.m
+++ +logic/+pointcloud/loadData.m
@@ -1,5 +1,9 @@
 function pc = loadData(path)
 % LOADDATA - load point cloud from massaged .txt file
+% Inputs:
+%   path - path to .txt file
+% Outputs:
+%   pc - point cloud object
 
 data = readmatrix(path);
 pc = pointCloud(data(:,2:4), "Color", data(:,5:7));
--- +logic/+pointcloud/segmentation.m
+++ +logic/+pointcloud/segmentation.m
@@ -1,19 +1,35 @@
-function [pcSeg, idx] = segmentation(pc, dist)
+function [pcSeg, idx, numClusters] = segmentation(pc, minDist, minN)
 % SEGMENTATION Segments a point cloud into clusters
-% pc: point cloud
-% dist: distance threshold for segmentation
-% pc_seg: segmented point cloud
-% idx: indices of the segments
+% Inputs:
+%   pc: point cloud
+%   minDist: minimum distance between clusters
+%   minN: minimum number of points in a cluster
+% Outputs:
+%   pcSeg: segmented point cloud
+%   idx: indices of the segments
+%   numClusters: number of clusters
 
-
-[labels, numClusters] = pcsegdist(pc, dist);
+[labels, numClusters] = pcsegdist(pc, minDist);
 
 pcSeg = cell(numClusters, 1);
 idx = cell(numClusters, 1);
 
 for i = 1:numClusters
-    idx{i} = find(labels == i);
-    pcSeg{i} = select(pc, idx{i});
+    tmp_idx = find(labels == i);
+
+    if length(tmp_idx) >= minN
+        idx{i} = tmp_idx;
+        pcSeg{i} = select(pc, idx{i});
+
+        % different random color for each cluster
+        pcSeg{i}.Color = repmat(rand(1, 3), length(tmp_idx), 1);
+    end
 end
+
+is_empty = cellfun(@isempty, pcSeg);
+idx = idx(~is_empty);
+pcSeg = pcSeg(~is_empty);
+
+numClusters = length(pcSeg);
 
 end
