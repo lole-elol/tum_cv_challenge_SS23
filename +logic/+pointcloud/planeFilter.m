b77e469dc9c375338d29e4caf9bffb450243a4d3
Lorenz Leinfelder <loleider@gmx.de>
2023-06-21 11:11:38 +0200

    add filtering for walls only

--- +logic/+pointcloud/planeFilter.m
+++ +logic/+pointcloud/planeFilter.m
@@ -1,26 +1,26 @@
 function [planes] = planeFilter(geoPlanes)
 % PLANEFILTER filters out planes that are not orthogonal to ecach other
+% or where the normal vector is close to [ 0 0 1]
 % geoPlanes: struct of planes with fileds: Parameters, Normal
 % planes: struct of planes with fileds: Parameters, Normal
 % tanaplh = sin(5)
 % atan2(norm(cross(a,b)), dot(a,b))
 % filter out planes that are not orthogonal to ecach other plane in geoPlanes
-geoPlanes = geoPlanes(cellfun(@(x) ~isempty(x), geoPlanes))
+geoPlanes = geoPlanes(cellfun(@(x) ~isempty(x), geoPlanes));
 planes = cell(1, length(geoPlanes));
 for i = 1:length(geoPlanes)
     for j = 1:length(geoPlanes)
         if i ~= j
-            if abs(dot(geoPlanes{i}.Normal, geoPlanes{j}.Normal)) < 0.1
+            if abs(dot(geoPlanes{i}.Normal, geoPlanes{j}.Normal)) < 0.0001
                 planes{i} = geoPlanes{i};
                 break;
             end
         end
     end
-    % if abs(cross(geoPlanes{i}.Normal, geoPlanes{j}.Normal)) < tanaplh
-    %     planes{i} = geoPlanes{i};
-    %     break;
-    
-    % for i = 1:length(geoPlanes)
-    %     geoPlanes{i}.Normal
-    % end
+end
+
+% filter out planes that are not orthogonal to [0 0 1]
+planes = planes(cellfun(@(x) ~isempty(x), planes));
+planes = planes(cellfun(@(x) abs(x.Normal(3)) < 0.7, planes));
+
 end
