b9a94e7d51b141502811a052ba98328265a45a2f
Daniel San José Pro <ge49wuw@mytum.de>
2023-07-01 12:48:44 +0200

    Modified run function to work with 3D reconstruction

--- +benchmark/run.m
+++ +benchmark/run.m
@@ -9,10 +9,14 @@ function run(configPath, inPath, outPath, varargin)
 %   testDetection = true: If true, run the 3D Model Detection benchmark
 
 p = inputParser;
+p.addRequired("configPath", @isstring)
+p.addRequired("inPath", @isstring)
+p.addRequired("outPath", @isstring)
 p.addParameter('testReconstruction', true, @islogical);
 p.addParameter('testDetection', true, @islogical);
 
-p.parse(varargin{:});
+p.parse(configPath, inPath, outPath, varargin{:});
+
 testReconstruction = p.Results.testReconstruction;
 testDetection = p.Results.testDetection;
 
@@ -22,13 +26,69 @@ load(configPath);
 
 if testReconstruction
     disp('Running 3D Reconstruction benchmark');
-    disp('! NOT IMPLEMENTED YET !')
+    
+    % TODO: Load input data from parameter
+    paths = ["test/old_computer", "test/delivery_area_dslr_undistorted"];
+    scenes = cell(length(paths), 1);
+    for i = 1:length(paths)
+        path = paths(i);
+        images = util.loadImages(path + '/images', numImages=5);  % TODO: load all images
+        cameraParams = logic.reconstruct3D.loadCameraParams(path + '/cameras.txt');
+        scenes{i} = {images, cameraParams};
+        fprintf("\n");
+    end
+    numScenes = length(scenes);
+
+    % Prepare all the combinations of parameters
+    fields = fieldnames(reconstruction);
+    combinationsArgs = cell(1, numel(fields));
+    for i = 1:numel(fields)
+        combinationsArgs{i} = reconstruction.(fields{i});
+    end
+    reconstructionParams = combinations(combinationsArgs{:});  % Table containing all combinations
+    reconstructionParams.Properties.VariableNames = fields;
+    numCombinations = size(reconstructionParams, 1);
+    % Save parameter combinations
+    outputMat = append(outPath, '/reconstructionParams.mat');
+    save(outputMat, 'reconstructionParams');
+
+    disp('Found ' + string(numCombinations) + ' parameter combinations:');
+    disp(reconstructionParams)
+    input('Press enter to continue ...');
+    disp('Running benchmark ...');
+    fprintf('\n')
+
+    % Run the benchmark by running the reconstruction algorithm with each
+    % parameter combination and each scene. Save the output data.
+    outData = cell(numCombinations, numScenes);  
+    for i=1:numCombinations
+        paramsTable = reconstructionParams(i, :);
+        disp('Running parameter combination ' + string(i));
+        fprintf('\n')
+        disp(paramsTable)
+        fprintf('\n')
+        params = table2cell(paramsTable);
+        for j = 1:numScenes
+            scene = scenes{j};
+            images = scene{1};
+            cameraParams = scene{2};
+            disp('Running scene ' + string(j) + ' of ' + string(numScenes));
+            tic
+            [pointCloudInstance, camPoses, tracks] = logic.reconstruct3DMultiview(images, cameraParams, params{:});
+            t = toc;
+            outData{i, j} = {pointCloudInstance, camPoses, tracks};
+            disp('Time: ' + string(t) + 's');
+        end
+    end
+    % Save output data
+    save(append(outPath, '/reconstructionOut.mat'), 'outData');
+    disp('3D Reconstruction benchmark done!')
 end
 
 if testDetection
     disp('Running 3D Model Detection benchmark');
     if ~testReconstruction
-        % Load input data
+        % Load input data, since it has not been computed before
         disp('Loading input point clouds');
         files = dir(append(inPath, '/*.txt'));
 
