2d8c8d9bb0eb2be18ffd0152d1357924f250d77a
Daniel San José Pro <ge49wuw@mytum.de>
2023-07-02 20:26:10 +0200

    correct plot funcion for cameras

--- +plotting/plotPointCloud.m
+++ +plotting/plotPointCloud.m
@@ -3,6 +3,7 @@ function plotPointCloud(pointCloudInstance, camPoses, varargin)
     % Inputs:
     %   pointCloudInstance: 3D point cloud as a pointCloud object
     %   camPoses: camera poses as an array of rigid3d objects
+    %   usePcViewer = false: use the pcviewer function instead of pcshow
     %   cameraSizePlotSize = 1.0: size of the camera plot
     %   pcMarkerSize = 45: size of the point cloud markers
     % Outputs:
@@ -10,9 +11,9 @@ function plotPointCloud(pointCloudInstance, camPoses, varargin)
 
     % Define parser
     p = inputParser;
-    p.addOptional('usePcViewer', false);
-    p.addOptional('cameraSizePlotSize', 1.0);
-    p.addOptional('pcMarkerSize', 45);
+    p.addOptional('usePcViewer', false, @islogical);
+    p.addOptional('cameraSizePlotSize', 1.0, @isnumeric);
+    p.addOptional('pcMarkerSize', 45, @isnumeric);
     p.parse(varargin{:});
     usePcViewer = p.Results.usePcViewer;
     cameraSizePlotSize = p.Results.cameraSizePlotSize;
@@ -25,15 +26,7 @@ function plotPointCloud(pointCloudInstance, camPoses, varargin)
     else
         pcshow(pointCloudInstance, VerticalAxis='Y', VerticalAxisDir='Down', MarkerSize=pcMarkerSize);
         % Show cameras poses
-        if istable(camPoses)
-            camPoses = camPoses(:, 2).AbsolutePose;
-        end
-        for pose=1:length(camPoses)
-            % Plot camera
-            camPose = camPoses(pose);
-            plotCamera(Location=camPose.Translation, Orientation=camPose.R, ...
-                Size=cameraSizePlotSize, Color='b', Opacity=0, label=string(pose));
-        end
+        plotCamera(camPoses);
         xlabel('X');
         ylabel('Y');
         zlabel('Z');
