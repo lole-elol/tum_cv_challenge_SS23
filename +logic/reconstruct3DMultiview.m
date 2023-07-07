e7c36c03226347d2504bb8344a08a3c850d48825
Daniel San José Pro <ge49wuw@mytum.de>
2023-07-07 09:21:21 +0200

    Further tests, not really usable still

--- +logic/reconstruct3DMultiview.m
+++ +logic/reconstruct3DMultiview.m
@@ -21,7 +21,7 @@ function [pointCloudInstance, camPoses, tracks] = reconstruct3DMultiview(images,
 p = inputParser;
 p.addOptional('log', true);
 %% Preprocessing Params
-p.addOptional('scalingFactor', 0.1);
+p.addOptional('scalingFactor', 0.25);
 %% Reconstruction Params
 % Feature extraction parameters
 p.addOptional('featureExtractionMethod', 'SURF');
@@ -163,53 +163,50 @@ if log
     fprintf('Dense reconstruction\n');
 end
 % Create a dense point cloud from the triangulated points and camera poses.
-for i = 1:1
+for i = 1:(numImages-1)
     if log
         fprintf('Dense reconstruction of image %d of %d \r', i, numImages-1);
     end
     image1 = imagesOriginal{i};
     image2 = imagesOriginal{i+1};
-    figure
-    imshowpair(image1, image2, 'montage');
+    % figure
+    % imshowpair(image1, image2, 'montage');
     
     % Compute stereo parameters and rectify the stereo images.
-    relPose = rigidtform3d(poses(vSet, i+1).AbsolutePose.A);
+    relPose = rigidtform3d(poses(vSet, i).AbsolutePose.A \ poses(vSet, i+1).AbsolutePose.A);
     stereoParams = stereoParameters(cameraParams, cameraParams, relPose);
     %[image1Rect, image2Rect] = rectifyStereoImages(rgb2gray(image1), rgb2gray(image2), stereoParams);
-    [image1Rect, image2Rect] = rectifyStereoImages(image1, image2, stereoParams);
-    imshowpair(image1Rect, image2Rect, 'montage');
+    [image1Rect, image2Rect, ~, camMatrix1] = rectifyStereoImages(image1, image2, stereoParams);
+    % imshowpair(image1Rect, image2Rect, 'montage');
     
     % Compute disparity.
     disparityMap = disparitySGM(rgb2gray(image1Rect), rgb2gray(image2Rect));
     % Remove left most column of zeros from disparity map and corresponding pixels from the rectified image.
-    figure
+    % figure
     % imshow(disparityMap);
     % colormap jet
     % colorbar
 
     % Reconstruct the 3-D world coordinates of points corresponding to each pixel from the disparity map.
     xyzPoints = reconstructScene(disparityMap, stereoParams);
-    numColumnsToRemove = 200;
-    xyzPoints = xyzPoints(:, numColumnsToRemove:end, :);
-    disparityMap = disparityMap(:, numColumnsToRemove:end);
-    % imshow(disparityMap);
-
     
-    % Filter points that are too far away and transform the MxNx3 matrix into a Nx3 matrix.
+    % Filter points that are too far away and transform the MxNx3 matrix into a Nx3 matrix
+    roiBorder = 50;  % TODO: make this a parameter
+    roi = zeros(size(disparityMap));
+    roi(roiBorder:end-roiBorder, roiBorder:end-roiBorder) = 1;
     xyzPoints = reshape(xyzPoints, [], 3);
-    pixelColors = reshape(image1Rect, [], 3);
-    size(xyzPoints)
-    size(pixelColors)
-    validIdx = xyzPoints(:, 3) < maxZ & xyzPoints(:, 3) > minZ;
+    roi = reshape(roi, [], 1);
+    % validIdx = xyzPoints(:, 3) < 1.5*maxZ & xyzPoints(:, 3) > 0.5*minZ & roi;
+    validIdx = xyzPoints(:, 3) < maxZ & roi;
     xyzPoints = xyzPoints(validIdx, :);
-    pixelColors = pixelColors(validIdx, :);
+    xyzPointsInImage1 = (camMatrix1 * [xyzPoints, ones(size(xyzPoints, 1), 1)]')';
+    xyzPointsInImage1 = floor(xyzPointsInImage1(:, 2:-1:1) ./ xyzPointsInImage1(:, 3));
+    pixelColors = impixel(image1Rect, xyzPointsInImage1(: , 1), xyzPointsInImage1(: , 2));
 
 
     % create a pointCloud object and assing the color of the original image at the corresponding pixel
     size(xyzPoints)
-    size(pixelColors)
-    % pointCloudInstance = pointCloud(xyzPoints, Color=pixelColors);
-    pointCloudInstance = pointCloud(xyzPoints);
+    pointCloudInstance = pccat([pointCloudInstance, pointCloud(xyzPoints, Color=pixelColors)]);
 end
 if log
     fprintf('\nDense reconstruction finished after %.2f seconds.\n', toc);
