abca46e8cc2440b20828a2c2dc54155b2f26e448
lole-elol <73332880+lole-elol@users.noreply.github.com>
2023-07-08 20:24:41 +0200

    Feat pipeline (#46)
    * Updated Branches
    
    * GUI Upload into branch feat_gui
    
    * move Gui to dir
    
    * ..
    
    * rm
    
    * Update GUI
    
    * Added Readme
    
    * Update Readme.txt
    
    * Update and rename Readme.txt to Readme.md
    
    * Update Readme.md
    
    Lukas showed me how to use markdown
    
    * GUI update
    
    * new GUI update
    
    * prevent double filtering
    
    * pipeline to be called from gui
    
    * add initial pipeline call
    
    ---------
    
    Co-authored-by: saminoel <saminoel@localhost>
    Co-authored-by: Sami Noel <saminoel@MacBook-Air.local>
    Co-authored-by: Sami <136233156+SamiNoelAzizHanna@users.noreply.github.com>
    Co-authored-by: saminoel <saminoel@macbook-air>
    Co-authored-by: Sami Noel <saminoel@macbook-air.speedport.ip>

--- +gui/createGui.m
+++ +gui/createGui.m
@@ -1,4 +1,2 @@
 function envHandle = createGUI()
-% Initialize the GUI
-
 end
---
+++ pipeline.m
@@ -0,0 +1,45 @@
+function [models, pc] = pipeline(images, camParams, roomHeigth)
+% PIPELINE main function of the pipeline triggered by the GUI
+% perforem teh computation of the 3D point cloud and the detection of the models
+% input:
+%   images: cell array of images
+%   camParams: camera parameters
+%   scalingFactor: scaling factor for the point cloud
+% output:
+%   models: cell array of detected models
+%   pc: point cloud
+
+
+% load hyper parameters
+load("config/paramsV1.mat");
+
+% reconstruct 3D point cloud from images
+[pointCloudInstance,~,~] = logic.reconstruct3DMultiview(images, camParams, reconstruction);
+
+
+%% Remove outliers
+pc = logic.pointcloud.filter(pointCloudInstance, detection.outlierDist);
+
+%% Detect floor and ceiling
+[~, pc, pcFloor, floorPlane] = logic.pointcloud.groundPlane(pc);
+pc = removeInvalidPoints(pc);
+
+% Rotate point cloud so that floor is horizontal
+pc = logic.pointcloud.rotate(pc, floorPlane.Normal);
+
+[ceilingPlane, pcCeiling, pc] = logic.pointcloud.ceilPlane(pc, maxDistance=detection.ceilingDist, percentage=detection.ceilingPercentile, refVector=floorPlane.Normal, windowSize= detection.ceilingWindowSize);
+
+% calculate scaling factor
+scalingFactor = logic.pointcloud.scalingFactorFromRoomHeight(floorPlane, ceilingPlane, roomHeigth);
+
+% scale point cloud via scaling factor
+tScaling = affinetform3d([scalingFactor, 0, 0, 0; 0, scalingFactor, 0, 0; 0, 0, scalingFactor, 0; 0, 0, 0, 1]);
+pc = pctransform(pointCloudInstance, tScaling);
+
+
+
+% detect models planes and Cuboids
+[models, pc, ~] = logic.modelDetection(pc, detection);
+
+
+end
