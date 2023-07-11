50db35058a5f19b2f685421832d19c15f546052b
Lukas Schüttler <lukas.schuettler@ise-technology.com>
2023-07-11 13:43:59 +0200

    fix image loading on linux, add gui starting to main

--- main.m
+++ main.m
@@ -1,10 +1,2 @@
-
-% Clear all variables and old windows
-close all force
-clear all
-
-%% Initialize the environment
-% Initialize GUI
-global envHandle;
-envHandle = env.createGUI();
-data = guidata(envHandle.UIFigure);
+%% Start Gui
+gui.GUI
